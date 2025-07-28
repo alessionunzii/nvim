762284f73b95b19b1001737a5b80efeb05017ac1
Alessio Nunzi <nunzi.alessio2006@gmail.com>
2025-07-28 16:53:22 +0200

    piccole modifiche

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,19 +1,15 @@
----@diagnostic disable: undefined-global
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.g.background = "light"
+vim.g.background = "dark"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.opt.swapfile = false
 
--- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.keymap.set('n', '<C-a>', ':%y +<CR>')
-vim.wo.number = true
+vim.keymap.set('n', '<C-a>', ':%y +<CR>') --Copia tutto il file
+vim.keymap.set('i', '<C-v>', '<C-r>+') --Incolla
