c8ecc8148900a8bf4ab133601e953d5359236c6b
eklax <nunzi.alessio2006@gmail.com>
2025-11-05 12:17:28 +0100

    Set Gruvbox as colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,37 @@
+-- Add this to your lazy.nvim plugins configuration
+-- Usually in ~/.config/nvim/lua/plugins/colorscheme.lua
+-- or in your main lazy setup file
+
 return {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function()
-        require("tokyonight").setup({
-            style = "night", -- storm, moon, night, day
-            transparent = true,
-            styles = {
-                comments = { italic = true },
-                keywords = { italic = true },
-                sidebars = "transparent",
-            },
-        })
-        vim.cmd("colorscheme tokyonight")
-    end,
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000, -- Load colorscheme before other plugins
+  config = function()
+    require("gruvbox").setup({
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "soft", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = true,
+    })
+    
+    -- Set the colorscheme
+    vim.cmd("colorscheme gruvbox")
+  end,
 }
