3cd10f1b160ab3d49d997b8e10bd3e6b7b72eff3
eklax <nunzi.alessio2006@gmail.com>
2026-02-01 17:45:51 +0100

    Fix treesitter

--- lua/plugins/tresitter.lua
+++ lua/plugins/tresitter.lua
@@ -3,11 +3,20 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
+      -- Try to load the module, but don't crash if it's missing
+      local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+      
+      -- If the plugin isn't found, stop here (prevents the error screen)
+      if not status_ok then
+        return
+      end
+
+      -- If found, proceed with setup
+      configs.setup({
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html" },
       })
     end
   }
