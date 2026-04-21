0f5c9269c96efd55faf6fa65ed861e33d56d4add
Alessio Nunzi <nunzi.alessio2006@gmail.com>
2026-04-21 17:49:49 +0200

    modificato shortcut

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
 vim.g.background = "dark"
 
@@ -9,8 +10,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.swapfile = false
 
+vim.keymap.set('n', '<C-n>', '<C-w>v', { desc = 'Split window vertically' })
+vim.keymap.set('v', '<C-c>', '"+y', { desc = 'Copy to system clipboard' })
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {})
 vim.keymap.set("n", "<leader>r", ":!javac % && cd %:h && java %:t:r<CR>")
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.keymap.set('n', '<C-a>', ':%y +<CR>') --Copia tutto il file
-vim.keymap.set('i', '<C-v>', '<C-r>+') --Incolla
+vim.keymap.set('n', '<C-a>', ':%y +<CR>', {desc = 'Copia contenuto dell intero file'})
