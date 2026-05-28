89e38feda2fb981af063a11171d0d3cbb089a3df
Alessio Nunzi <nunzi.alessio2006@gmail.com>
2026-05-28 16:30:08 +0200

    cambiato colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,38 +1,27 @@
 return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000, -- Load colorscheme before other plugins
+  "folke/tokyonight.nvim",
+  priority = 1000,
   config = function()
-    require("kanagawa").setup({
-      compile = false,             -- enable compiling the colorscheme
-      undercurl = true,            -- enable undercurls
-      commentStyle = { italic = false },
-      functionStyle = { italic = false},
-      keywordStyle = { italic = false},
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = true,         -- do not set background color
-      dimInactive = false,         -- dim inactive window :h hl-NormalNC
-      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-      colors = {                   -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    require("tokyonight").setup({
+      style = "night",
+      transparent = true,
+      terminal_colors = true,
+      styles = {
+        comments = { italic = false },
+        keywords = { italic = false },
+        functions = {},
+        variables = {},
+        sidebars = "transparent",
+        floats = "transparent",
       },
-      overrides = function(colors) -- add/modify highlights
-        return {
-          LineNr = {bg = "NONE"};
-          LineNrAbove = {bg = "NONE"};
-          LineNrBelow = {bg = "NONE"};
-          SignColumn = {bg = "NONE"};
-          FoldColumn = {bg = "NONE"};
-        }
+      on_highlights = function(hl, _)
+        hl.LineNr = { bg = "NONE" }
+        hl.LineNrAbove = { bg = "NONE" }
+        hl.LineNrBelow = { bg = "NONE" }
+        hl.SignColumn = { bg = "NONE" }
+        hl.FoldColumn = { bg = "NONE" }
       end,
-      theme = "dragon",              -- Load "wave" theme when 'background' option is not set
-      background = {               -- map the value of 'background' option to a theme
-        dark = "dragon",             -- try "dragon" !
-        light = "dragon"
-      },
     })
-    -- Set the colorscheme
-    vim.cmd("colorscheme kanagawa")
+    vim.cmd("colorscheme tokyonight")
   end,
 }
